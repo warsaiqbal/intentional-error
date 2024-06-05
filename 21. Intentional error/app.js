8beff5d5a0aaeb985484bdb2b7ad4d8019255f42
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:13:56 -0700

    Add files via upload

---
+++ 21. Intentional error/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let countries = ["usa", "england", "pakistan"];
+//console.log(countries[3]); //Intentional error
+console.log(countries[0], countries[1], countries[2]); //error fixed
---
+++ 21. Intentional error/app.ts
@@ -0,0 +1,4 @@
+let countries: string[] = ["usa", "england", "pakistan"]
+//console.log(countries[3]); //Intentional error
+
+console.log(countries[0], countries[1], countries[2]); //error fixed
